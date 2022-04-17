864b9cec2f3b5781eb04945a8b9b1be544f63604
SecondThundeR <awayfromgalaxy@gmail.com>
2022-04-17 21:04:15 +0300

    update anime enums file
    rename to anime.py
    remove trailing commas
    add docstrings

--- shikithon/enums/Anime.py
+++ shikithon/enums/Anime.py
@@ -1,62 +1,70 @@
+"""Enums for /api/animes."""
 from enum import Enum
 
 
 class Order(Enum):
+    """Contains variables related for list ordering query."""
     NONE = ""
-    ID = "id",
-    RANKED = "ranked",
-    KIND = "kind",
-    POPULARITY = "popularity",
-    NAME = "name",
-    AIRED_ON = "aired_on",
-    EPISODES = "episodes",
-    STATUS = "status",
+    ID = "id"
+    RANKED = "ranked"
+    KIND = "kind"
+    POPULARITY = "popularity"
+    NAME = "name"
+    AIRED_ON = "aired_on"
+    EPISODES = "episodes"
+    STATUS = "status"
     RANDOM = "random"
 
 class Kind(Enum):
+    """Contains variables related for getting certain kind of anime."""
     NONE = ""
     TV = "tv"
-    TV_13 = "tv_13",
-    TV_24 = "tv_24",
-    TV_48 = "tv_48",
-    MOVIE = "movie",
-    OVA = "ova",
-    ONA = "ona",
-    SPECIAL = "special",
+    TV_13 = "tv_13"
+    TV_24 = "tv_24"
+    TV_48 = "tv_48"
+    MOVIE = "movie"
+    OVA = "ova"
+    ONA = "ona"
+    SPECIAL = "special"
     MUSIC = "music"
 
 class Status(Enum):
+    """Contains variables related for getting certain status of anime."""
     NONE = ""
-    ANONS = "anons",
+    ANONS = "anons"
     ONGOING = "ongoing"
     RELEASED = "released"
     EPISODE = "episode"
 
 class Duration(Enum):
+    """Contains variables related for getting certain duration of anime."""
     NONE = ""
     SHORT = "S"
     MEDIUM = "D"
     LONG = "F"
 
 class Rating(Enum):
+    """Contains variables related for getting certain rating of anime."""
     NONE = ""
     NO_RATING = "none"
-    ALL_AGES = "g",
+    ALL_AGES = "g"
     CHILDREN = "pg"
-    TEENS = "pg_13",
+    TEENS = "pg_13"
     VIOLENCE = "r"
     MILD_NUDITY = "r_plus"
     HENTAI = "rx"
 
 class Censorship(Enum):
+    """Contains variables related for getting certain censorship status of anime."""
     CENSORED = "true"
     UNCENSORED = "false"
 
 class MyList(Enum):
-    NONE = "",
-    PLANNED = "planned",
-    WATCHING = "watching",
-    REWATCHING = "rewatching",
-    COMPLETED = "completed",
-    ON_HOLD = "on_hold",
+    """Contains variables related for getting certain user list status of anime."""
+    NONE = ""
+    PLANNED = "planned"
+    WATCHING = "watching"
+    REWATCHING = "rewatching"
+    COMPLETED = "completed"
+    ON_HOLD = "on_hold"
     DROPPED = "dropped"
