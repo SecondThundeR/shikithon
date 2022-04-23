cb44fb41fd51a4817ccf269c4b2b9ac66f5504dd
SecondThundeR <awayfromgalaxy@gmail.com>
2022-04-23 18:15:51 +0300

    fix user_rate_score value type
    As fixed in the main API https://github.com/shikimori/shikimori/issues/2408

--- shikithon/models/user_rate_score.py
+++ shikithon/models/user_rate_score.py
@@ -1,15 +1,9 @@
 """Submodel for anime.py"""
 # pylint: disable=E0611, R0903, E0402 (no-name-in-module, too-few-public-methods, relative-beyond-top-level)
-from typing import Union
-
 from pydantic import BaseModel
 
 
 class UserRateScore(BaseModel):
-    """Represents user rate score entity.
-
-    Because of the unusual behavior of the Shikimori API,
-    it can return either int or float as a result.
-    """
+    """Represents user rate score entity."""
     name: int
-    value: Union[int, float]
+    value: int
