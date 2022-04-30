18143c744fb57c341ed261877562643801a44075
SecondThundeR <awayfromgalaxy@gmail.com>
2022-04-30 19:21:28 +0300

    add utils module

---
+++ shikithon/utils.py
@@ -0,0 +1,30 @@
+"""
+Set of utility methods for the shikithon library.
+
+This file contains the Utils class
+with all the necessary utility methods
+to work with the library
+"""
+from typing import Dict
+
+
+class Utils:
+    """
+    Utils class.
+
+    Contains all the necessary utility methods
+    to work with the library
+    """
+    @staticmethod
+    def prepare_query_dict(query_dict: Dict[str, str]) -> str:
+        """
+        Convert query dict to query string for link.
+
+        :param Dict[str, str] query_dict: Query dictionary
+        :return: Query string
+        :rtype: str
+        """
+        query_dict_str = "&".join(
+            f"{key}={val}" for (key, val) in query_dict.items()
+        )
+        return f"?{query_dict_str}"
