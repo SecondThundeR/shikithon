609e8a046b40848babfa85ea926f035d8064fca2
SecondThundeR <awayfromgalaxy@gmail.com>
2022-05-04 19:57:32 +0300

    update utils
    add new utils methods:
    - get_new_expire_time - utility method for generating new token expire time (Used in APIs _cache_config method)
    - generate_query_dict - special method for generating correct query for API request
    fix docstrings

--- shikithon/utils.py
+++ shikithon/utils.py
@@ -5,7 +5,11 @@ This file contains the Utils class
 with all the necessary utility methods
 to work with the library
 """
+from time import time
+from enum import Enum
+from typing import List
 from typing import Dict
+from typing import Union
 
 
 class Utils:
@@ -32,10 +36,49 @@ class Utils:
     @staticmethod
     def convert_app_name(app_name: str) -> str:
         """
-        Api Test -> api_test
+        Converts app name to snake case for use in
+        config cache filename.
 
-        :param app_name:
-        :return: Converted app name for config cache filename
+        :param app_name: Current OAuth app name
+        :return: Converted app name for filename
         :rtype: str
         """
         return "_".join(app_name.lower().split(" "))
+
+    @staticmethod
+    def get_new_expire_time(time_expire_constant: int) -> int:
+        """
+        Generates new token expire time.
+
+        :param time_expire_constant: Token lifetime value
+        :return: New token expire time
+        :rtype: int
+        """
+        return int(time()) + time_expire_constant
+
+    @staticmethod
+    def generate_query_dict(
+            **params_data: Dict[str, Union[int, Enum, List[int]]]
+    ) -> Dict[str, str]:
+        """
+        Returns valid query dict for API requests.
+
+        This methods checks for data type and converts to valid one.
+
+        :param Dict[str, Union[int, Enum, List[int]]] params_data:
+            API methods parameters data
+        :return: Valid query dictionary
+        :rtype: Dict[str, str]
+        """
+        new_query: Dict[str, str] = {}
+        for key, data in params_data.items():
+            if isinstance(data, int):
+                new_query[key] = str(data)
+            if isinstance(data, Enum):
+                new_query[key] = data.value
+            if isinstance(data, list):
+                data = [
+                    str(value) if value.isdigit() else value for value in data
+                ]
+                new_query[key] = ",".join(data)
+        return new_query
