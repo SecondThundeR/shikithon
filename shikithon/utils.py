74b0571a21dd20e55e97192f634540b6c755206c
SecondThundeR <awayfromgalaxy@gmail.com>
2022-05-10 01:47:26 +0300

    update utils
    run pre-commit hooks
    fix docstrings

--- shikithon/utils.py
+++ shikithon/utils.py
@@ -5,11 +5,9 @@ This file contains the Utils class
 with all the necessary utility methods
 to work with the library
 """
-from time import time
 from enum import Enum
-from typing import List
-from typing import Dict
-from typing import Union
+from time import time
+from typing import Dict, List, Union
 
 LOWER_LIMIT_NUMBER = 1
 
@@ -21,19 +19,21 @@ class Utils:
     Contains all the necessary utility methods
     to work with the library
     """
+
     @staticmethod
     def prepare_query_dict(query_dict: Dict[str, str]) -> str:
         """
         Convert query dict to query string for link.
 
-        :param Dict[str, str] query_dict: Query dictionary
+        :param query_dict: Query dictionary
+        :type query_dict: Dict[str, str]
+
         :return: Query string
         :rtype: str
         """
-        query_dict_str = "&".join(
-            f"{key}={val}" for (key, val) in query_dict.items()
-        )
-        return f"?{query_dict_str}"
+        query_dict_str = '&'.join(
+            f'{key}={val}' for (key, val) in query_dict.items())
+        return f'?{query_dict_str}'
 
     @staticmethod
     def convert_app_name(app_name: str) -> str:
@@ -42,10 +42,12 @@ class Utils:
         config cache filename.
 
         :param app_name: Current OAuth app name
+        :type app_name: str
+
         :return: Converted app name for filename
         :rtype: str
         """
-        return "_".join(app_name.lower().split(" "))
+        return '_'.join(app_name.lower().split(' '))
 
     @staticmethod
     def get_new_expire_time(time_expire_constant: int) -> int:
@@ -53,6 +55,8 @@ class Utils:
         Generates new token expire time.
 
         :param time_expire_constant: Token lifetime value
+        :type time_expire_constant: int
+
         :return: New token expire time
         :rtype: int
         """
@@ -60,15 +64,16 @@ class Utils:
 
     @staticmethod
     def generate_query_dict(
-            **params_data: Union[str, bool, int, Enum, List[int], None]
+        **params_data: Union[str, bool, int, Enum, List[int], None]
     ) -> Dict[str, str]:
         """
         Returns valid query dict for API requests.
 
         This methods checks for data type and converts to valid one.
 
-        :param Union[str, bool, int, Enum, List[int], None] params_data:
-            API methods parameters data
+        :param params_data: API methods parameters data
+        :type params_data: Union[str, bool, int, Enum, List[int], None]
+
         :return: Valid query dictionary
         :rtype: Dict[str, str]
         """
@@ -78,7 +83,7 @@ class Utils:
                 continue
             if isinstance(data, bool):
                 if data is True:
-                    new_query[key]= str(int(data))
+                    new_query[key] = str(int(data))
                 else:
                     continue
             elif isinstance(data, int):
@@ -89,24 +94,26 @@ class Utils:
                 data = [
                     str(value) if value.isdigit() else value for value in data
                 ]
-                new_query[key] = ",".join(data)
+                new_query[key] = ','.join(data)
             else:
                 new_query[key] = data
         return new_query
 
     @staticmethod
-    def validate_query_number(
-            query_number: Union[int, None],
-            upper_limit: int
-    ) -> Union[int, None]:
+    def validate_query_number(query_number: Union[int, None],
+                              upper_limit: int) -> Union[int, None]:
         """
         Validates query number.
 
         If number is not in range, returns lower limit number,
         otherwise number or None.
 
-        :param Union[int, None] query_number: Number to validate
-        :param int upper_limit: Upper limit for range check
+        :param query_number: Number to validate
+        :type query_number: Union[int, None]
+
+        :param upper_limit: Upper limit for range check
+        :type upper_limit: int
+
         :return: Validated value
         :rtype: Union[int, None]
         """
