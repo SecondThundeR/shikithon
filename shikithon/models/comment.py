779582320c3b2a7d69f0a560afa66cecd0c3acd7
SecondThundeR <awayfromgalaxy@gmail.com>
2022-05-10 10:15:35 +0300

    remove is_summary field from comment.py

--- shikithon/models/comment.py
+++ shikithon/models/comment.py
@@ -1,6 +1,5 @@
 """Submodel for ban.py"""
 from datetime import datetime
-from typing import Optional
 
 from pydantic import BaseModel
 
@@ -14,7 +13,4 @@ class Comment(BaseModel):
     user_id: int
     created_at: datetime
     updated_at: datetime
-    # Seems like is_summary was deleted for sure from Shikimori API
-    # This field will be removed from here within a month
-    is_summary: Optional[bool]
     is_offtopic: bool
