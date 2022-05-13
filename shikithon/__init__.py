bef1c02caf1baa3f989a807a6628a67c7cc1860e
SecondThundeR <awayfromgalaxy@gmail.com>
2022-05-13 19:07:41 +0300

    bump version to 0.1.0-beta.11 and update pylint

--- shikithon/__init__.py
+++ shikithon/__init__.py
@@ -1,5 +1,5 @@
 """Contains package version and some magic for importing API object."""
 from shikithon.api import API
 
-__version__ = '0.1.0-beta.10'
+__version__ = '0.1.0-beta.11'
 __all__ = ['API']
