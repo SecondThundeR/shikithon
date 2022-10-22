cc164236d3dbcf868153e9d528705e057ccb10da
SecondThundeR <awayfromgalaxy@gmail.com>
2022-10-22 19:47:09 +0300

    feat(utils): update validate_return_data
    Add case for list with string, contains "Invalid parameter..." which returns new parameter: fallback (value to return on such case)
    Update :rtype: and fix docstring

--- shikithon/utils/utils.py
+++ shikithon/utils/utils.py
@@ -309,8 +309,10 @@ class Utils:
         response_data: Union[List[Dict[str, Any]], Dict[str, Any], List[Any],
                              int],
         data_model: Optional[Type[Any]] = None,
-        response_code: Optional[ResponseCode] = None
-    ) -> Optional[Union[Type[Any], List[Type[Any]], List[Any], bool]]:
+        response_code: Optional[ResponseCode] = None,
+        fallback: Optional[Any] = None
+    ) -> Optional[Union[Type[Any], List[Type[Any]], List[Any], Dict[str, Any],
+                        bool]]:
         """
         Validates passed response data and returns
         parsed models.
@@ -326,11 +328,16 @@ class Utils:
             (Used only when response_data is int)
         :type response_code: Optional[ResponseCode]
 
+        :param fallback: Fallback value to return
+        :type fallback: Optional[Any]
+
         :return: Parsed response data
-        :rtype: Optional[Union[Type[Any], List[Type[Any]], bool]]
+        :rtype: Optional[Union[Type[Any], List[Type[Any]],
+            List[Any], Dict[str, Any], bool]]
         """
         logger.debug(f'Validating return data: {response_data=}, '
                      f'{data_model=}, {response_code=}')
+
         if not response_data:
             logger.debug('Response data is empty. Returning it...')
             return response_data
@@ -339,6 +346,14 @@ class Utils:
             logger.debug('Response data is int. Returning value '
                          'of response code comparison')
             return response_data == response_code.value
+
+        if isinstance(response_data, list) \
+                and len(response_data) == 1 \
+                and response_data[0].find('Invalid parameter') != -1:
+            logger.debug('Response data contains invalid parameter info. '
+                         'Returning fallback value')
+            logger.warning(response_data[0])
+            return fallback
 
         if 'errors' in response_data or 'code' in response_data:
             logger.debug('Response data contains errors info. Returning None')
