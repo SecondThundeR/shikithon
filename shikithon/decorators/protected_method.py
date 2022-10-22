18e99edc1f21a55406776c6b4ced9752ec450502
SecondThundeR <awayfromgalaxy@gmail.com>
2022-10-22 13:05:37 +0300

    fix: slightly rewritten protected_method decorator for usage with resources
    Now, it is necessary to pass client_attr (e.g. "_client") to supress PyLint's W0212 (protected-access) warning because of accessing private _client attribute

--- shikithon/decorators/protected_method.py
+++ shikithon/decorators/protected_method.py
@@ -1,17 +1,14 @@
 """Decorator for protected methods"""
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Optional
+from typing import Optional
 
 from loguru import logger
 
 from ..resources.base_resource import BaseResource
 
-if TYPE_CHECKING:
-    from ..api import API
 
-
-def protected_method(scope: Optional[str] = None):
+def protected_method(client_attr: str, scope: Optional[str] = None):
     """
     Decorator for protected API methods.
 
@@ -24,11 +21,21 @@ def protected_method(scope: Optional[str] = None):
     Also, this decorator take a scope parameter
     for checking, if current app is allowed to access
     protected method.
+
+    :param client_attr: Name of client attribute
+    :type client_attr: str
+
+    :param scope: Scope of app
+    :type scope: Optional[str]
+
+    :return: Decorator function
+    :rtype: Callable
     """
 
     def protected_method_decorator(function):
 
-        def protected_method_wrapper(resource: BaseResource, *args, **kwargs):
+        def protected_method_wrapper(self, resource: BaseResource, *args,
+                                     **kwargs):
             """
             Decorator's wrapper function.
 
@@ -37,25 +44,28 @@ def protected_method(scope: Optional[str] = None):
 
             :return: None if API object is in restricted mode
                 or if required scope is missing
-            :rtype: None
+            :rtype: None or Callable
             """
+            client = getattr(self, client_attr)
             logger.debug('Checking the possibility of using a protected method')
-            if resource.client.restricted_mode:
+
+            if client.restricted_mode:
                 logger.debug('It is not possible to use the protected method '
                              'due to the restricted mode')
                 return None
 
-            if scope and scope not in resource.client.scopes_list:
+            if scope and scope not in client.scopes_list:
                 logger.debug(f'Protected method cannot be used due to the '
                              f'absence of "{scope}" scope')
                 return None
 
-            if resource.client.token_expired():
+            if client.token_expired():
                 logger.debug('Token has expired. Refreshing...')
-                resource.client.refresh_tokens()
+                client.refresh_tokens()
+
             logger.debug('All checks for use of the protected '
                          'method have been passed')
-            return function(resource, *args, **kwargs)
+            return function(self, resource, *args, **kwargs)
 
         return protected_method_wrapper
 
