d1fe3081f3948f785beeb3405256e6b65df0616b
SecondThundeR <awayfromgalaxy@gmail.com>
2022-11-05 20:02:25 +0300

    fix(Friends): update methods
    Add missing return typing
    Add fallback values
    Rename friend_id to user_id

--- shikithon/resources/friends.py
+++ shikithon/resources/friends.py
@@ -15,37 +15,37 @@ class Friends(BaseResource):
     """
 
     @method_endpoint('/api/friends/:id')
-    @protected_method('_client', 'friends')
-    async def create(self, friend_id: int):
+    @protected_method('_client', 'friends', fallback=False)
+    async def create(self, user_id: int) -> bool:
         """
         Creates (adds) new friend by ID.
 
-        :param friend_id: ID of a friend to create (add)
-        :type friend_id: int
+        :param user_id: ID of a user to create (add)
+        :type user_id: int
 
         :return: Status of create (addition)
         :rtype: bool
         """
         response: Union[Dict[str, Any], int] = await self._client.request(
-            self._client.endpoints.friend(friend_id),
+            self._client.endpoints.friend(user_id),
             headers=self._client.authorization_header,
             request_type=RequestType.POST)
-        return Utils.validate_response_data(response)
+        return Utils.validate_response_data(response, fallback=False)
 
     @method_endpoint('/api/friends/:id')
-    @protected_method('_client', 'friends')
-    async def destroy(self, friend_id: int):
+    @protected_method('_client', 'friends', fallback=False)
+    async def destroy(self, user_id: int) -> bool:
         """
         Destroys (removes) current friend by ID.
 
-        :param friend_id: ID of a friend to destroy (remove)
-        :type friend_id: int
+        :param user_id: ID of a user to destroy (remove)
+        :type user_id: int
 
         :return: Status of destroy (removal)
         :rtype: bool
         """
         response: Union[Dict[str, Any], int] = await self._client.request(
-            self._client.endpoints.friend(friend_id),
+            self._client.endpoints.friend(user_id),
             headers=self._client.authorization_header,
             request_type=RequestType.DELETE)
-        return Utils.validate_response_data(response)
+        return Utils.validate_response_data(response, fallback=False)
