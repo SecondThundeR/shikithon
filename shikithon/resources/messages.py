566917e9b6d8f9076ee1a160001160532a401cc5
SecondThundeR <awayfromgalaxy@gmail.com>
2022-11-05 22:13:19 +0300

    fix(Messages): add fallback values and update docs

--- shikithon/resources/messages.py
+++ shikithon/resources/messages.py
@@ -76,7 +76,7 @@ class Messages(BaseResource):
         :param body: New body of message
         :type body: str
 
-        :return: Updated message info
+        :return: Updated message info or None if message cannot be updated
         :rtype: Optional[Message]
         """
         response: Dict[str, Any] = await self._client.request(
@@ -87,7 +87,7 @@ class Messages(BaseResource):
         return Utils.validate_response_data(response, data_model=Message)
 
     @method_endpoint('/api/messages/:id')
-    @protected_method('_client', 'messages')
+    @protected_method('_client', 'messages', fallback=False)
     async def delete(self, message_id: int) -> bool:
         """
         Deletes message.
@@ -103,18 +103,15 @@ class Messages(BaseResource):
             headers=self._client.authorization_header,
             request_type=RequestType.DELETE)
         return Utils.validate_response_data(
-            response, response_code=ResponseCode.NO_CONTENT)
+            response, response_code=ResponseCode.NO_CONTENT, fallback=False)
 
     @method_endpoint('/api/messages/mark_read')
-    @protected_method('_client', 'messages')
+    @protected_method('_client', 'messages', fallback=False)
     async def mark_read(self,
                         message_ids: Optional[Union[int, List[int]]] = None,
                         is_read: Optional[bool] = None) -> bool:
         """
         Marks read/unread selected messages.
-
-        This method uses generate_query_dict for data dict,
-        because there is no need for nested dictionary
 
         :param message_ids: ID(s) of messages to mark read/unread
         :type message_ids: Optional[Union[int, List[int]]]
@@ -131,16 +128,14 @@ class Messages(BaseResource):
             data=Utils.create_query_dict(ids=message_ids, is_read=is_read),
             request_type=RequestType.POST)
         return Utils.validate_response_data(response,
-                                            response_code=ResponseCode.SUCCESS)
+                                            response_code=ResponseCode.SUCCESS,
+                                            fallback=False)
 
     @method_endpoint('/api/messages/read_all')
-    @protected_method('_client', 'messages')
+    @protected_method('_client', 'messages', fallback=False)
     async def read_all(self, message_type: str) -> bool:
         """
         Reads all messages on current user's account.
-
-        This method uses generate_query_dict for data dict,
-        because there is no need for nested dictionary
 
         **Note:** This methods accepts as type only 'news' and
         'notifications'
@@ -160,16 +155,14 @@ class Messages(BaseResource):
             data=Utils.create_query_dict(type=message_type),
             request_type=RequestType.POST)
         return Utils.validate_response_data(response,
-                                            response_code=ResponseCode.SUCCESS)
+                                            response_code=ResponseCode.SUCCESS,
+                                            fallback=False)
 
     @method_endpoint('/api/messages/delete_all')
-    @protected_method('_client', 'messages')
+    @protected_method('_client', 'messages', fallback=False)
     async def delete_all(self, message_type: str) -> bool:
         """
         Deletes all messages on current user's account.
-
-        This method uses generate_query_dict for data dict,
-        because there is no need for nested dictionary
 
         **Note:** This methods accepts as type only 'news' and
         'notifications'
@@ -189,4 +182,5 @@ class Messages(BaseResource):
             data=Utils.create_query_dict(type=message_type),
             request_type=RequestType.POST)
         return Utils.validate_response_data(response,
-                                            response_code=ResponseCode.SUCCESS)
+                                            response_code=ResponseCode.SUCCESS,
+                                            fallback=False)
