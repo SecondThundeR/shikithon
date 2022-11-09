e3608a446781e376c9d8908dd8d6a0fea6678f9e
SecondThundeR <awayfromgalaxy@gmail.com>
2022-11-10 02:31:26 +0300

    fix(Exceptions): update __init__.py

--- shikithon/exceptions/__init__.py
+++ shikithon/exceptions/__init__.py
@@ -1,14 +1,17 @@
 """Exceptions for shikithon API class.
 
 Used for raising on validation of config/app data.
+Only RetryLater exception is used for raising on request errors.
 """
 
 from .access_token_exception import AccessTokenException
-from .missing_app_variable import MissingAppVariable
-from .missing_auth_code import MissingAuthCode
-from .missing_config_data import MissingConfigData
+from .missing_app_variable import MissingAppVariableException
+from .missing_auth_code import MissingAuthCodeException
+from .missing_config_data import MissingConfigDataException
+from .retry_later import RetryLaterException
 
 __all__ = [
-    'AccessTokenException', 'MissingAuthCode', 'MissingAppVariable',
-    'MissingConfigData'
+    'AccessTokenException', 'MissingAuthCodeException',
+    'MissingAppVariableException', 'MissingConfigDataException',
+    'RetryLaterException'
 ]
