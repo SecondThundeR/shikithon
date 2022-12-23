b6596db780620818d8a91ec9c5bd3c52d4d59e8f
ren3104 <2ren3104@gmail.com>
2022-12-23 09:04:45 +0300

    Add more fields for manga model

--- shikithon/models/manga.py
+++ shikithon/models/manga.py
@@ -3,7 +3,11 @@ from typing import List, Optional
 
 from pydantic import BaseModel
 
+from .genre import Genre
 from .image import Image
+from .user_rate import UserRate
+from .user_rate_score import UserRateScore
+from .user_rate_status import UserRateStatus
 
 
 class Manga(BaseModel):
@@ -20,5 +24,23 @@ class Manga(BaseModel):
     chapters: int
     aired_on: Optional[str]
     released_on: Optional[str]
-    roles: Optional[List[str]]
-    role: Optional[str]
+    english: Optional[List[Optional[str]]]
+    japanese: Optional[List[Optional[str]]]
+    synonyms: Optional[List[Optional[str]]]
+    license_name_ru: Optional[str]
+    description: Optional[str]
+    description_html: Optional[str]
+    description_source: Optional[str]
+    franchise: Optional[str]
+    favoured: Optional[bool]
+    anons: Optional[bool]
+    ongoing: Optional[bool]
+    thread_id: Optional[int]
+    topic_id: Optional[int]
+    myanimelist_id: Optional[int]
+    rates_scores_stats: Optional[List[UserRateScore]]
+    rates_statuses_stats: Optional[List[UserRateStatus]]
+    licensors: Optional[List[str]]
+    genres: Optional[List[Genre]]
+    publishers: Optional[List[str]]
+    user_rate: Optional[UserRate]
