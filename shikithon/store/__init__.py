683f68f2fbf3646dcd2e2246c81780110ab34149
ren3104 <2ren3104@gmail.com>
2023-01-14 10:51:06 +0300

    Remove obsolete code

--- shikithon/store/__init__.py
+++ shikithon/store/__init__.py
@@ -1,8 +1,7 @@
 """Config store for shikithon library."""
 
 from .base import Store
-from .config_store import ConfigStore
 from .json import JsonStore
 from .memory import MemoryStore
 
-__all__ = ['ConfigStore', 'Store', 'MemoryStore', 'JsonStore']
+__all__ = ['Store', 'MemoryStore', 'JsonStore']
