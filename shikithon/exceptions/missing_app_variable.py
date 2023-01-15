4da261ad35f6b800491f4e489d8f4cd2693f7679
SecondThundeR <awayfromgalaxy@gmail.com>
2023-01-15 17:33:12 +0300

    feat(MissingAppVariable): add support for string array
    It is now possible to pass several variable names that are missing

--- shikithon/exceptions/missing_app_variable.py
+++ shikithon/exceptions/missing_app_variable.py
@@ -1,9 +1,15 @@
 """Exception for raising on missing app variable."""
+from typing import List, Union
 
 
 class MissingAppVariable(Exception):
 
-    def __init__(self, variable_name: str):
-        super().__init__(f'It is impossible to initialize an API object'
-                         f'without missing variable "{variable_name}". '
-                         f'Recheck your config and try again.')
+    def __init__(self, variable_name: Union[str, List[str]]):
+        exception_msg = 'Missing app variable(s): '
+        if isinstance(variable_name, list):
+            exception_msg += ', '.join(variable_name)
+        else:
+            exception_msg += variable_name
+        super().__init__('There is a problem with some app variables. '
+                         'Recheck your config and try again.\n'
+                         f'{exception_msg}')
