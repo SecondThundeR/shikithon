7909a25a63ba85931baf8b4ee66e565282fecf65
SecondThundeR <awayfromgalaxy@gmail.com>
2023-03-10 13:12:53 +0300

    fix(exceptions): change logger level to warning in RetryLater exception

--- shikithon/exceptions/retry_later.py
+++ shikithon/exceptions/retry_later.py
@@ -6,5 +6,5 @@ from loguru import logger
 class RetryLater(Exception):
 
     def __init__(self, error_message: str):
-        logger.error(error_message)
+        logger.warning(error_message)
         super().__init__(error_message)
