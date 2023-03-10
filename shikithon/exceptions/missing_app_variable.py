b4d8922a207382170116f15ed4874c096f4e0fe0
SecondThundeR <awayfromgalaxy@gmail.com>
2023-03-10 13:15:14 +0300

    refactor(exceptions): update MissingAppVariable exception

--- shikithon/exceptions/missing_app_variable.py
+++ shikithon/exceptions/missing_app_variable.py
@@ -9,12 +9,14 @@ class MissingAppVariable(Exception):
 
     def __init__(self, variable_name: Union[str, List[str]]):
         missing_variables = 'Missing app variable(s): '
+
         if isinstance(variable_name, list):
             missing_variables += ', '.join(variable_name)
         else:
             missing_variables += variable_name
-        msg = 'There is a problem with some app variables. ' \
+
+        error_message = 'There is a problem with some app variables. ' \
               'Recheck your config and try again.\n' \
               f'{missing_variables}'
-        logger.error(msg)
-        super().__init__(msg)
+        logger.error(error_message)
+        super().__init__(error_message)
