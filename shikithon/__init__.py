dbd4029eed67d3d2cd6df575cc7d51d4da17d74b
SecondThundeR <awayfromgalaxy@gmail.com>
2023-03-10 09:34:46 +0300

    chore: bump package version to 2.4.0

--- shikithon/__init__.py
+++ shikithon/__init__.py
@@ -5,5 +5,5 @@ from .store import MemoryStore
 from .store import NullStore
 from .store import Store
 
-__version__ = '2.3.1'
+__version__ = '2.4.0'
 __all__ = ['ShikimoriAPI', 'Store', 'NullStore', 'MemoryStore', 'JSONStore']
