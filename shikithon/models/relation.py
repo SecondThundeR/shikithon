848900fa8ed2094a71934e2f157fe9a8870b1b8a
SecondThundeR <awayfromgalaxy@gmail.com>
2023-03-12 15:49:45 +0300

    feat(models): add support for `Ranobe` model in `Relation.manga` field

--- shikithon/models/relation.py
+++ shikithon/models/relation.py
@@ -1,15 +1,16 @@
 """Model for /api/animes|mangas|ranobe/:id/related"""
-from typing import Optional
+from typing import Optional, Union
 
 from pydantic import BaseModel
 
 from .anime import Anime
 from .manga import Manga
+from .ranobe import Ranobe
 
 
 class Relation(BaseModel):
-    """Represents relation entity for anime."""
+    """Represents relation entity."""
     relation: str
     relation_russian: str
     anime: Optional[Anime]
-    manga: Optional[Manga]
+    manga: Optional[Union[Manga, Ranobe]]
