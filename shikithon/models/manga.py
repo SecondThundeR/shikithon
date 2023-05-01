3b4bbd69a178778b622d122820e24caeb1174bb7
SecondThundeR <awayfromgalaxy@gmail.com>
2023-05-01 21:06:34 +0300

    refactor(models): update Manga model fields
    Make MANGAS_KIND constant as tuple of strings
    Make some fields non-optional

--- shikithon/models/manga.py
+++ shikithon/models/manga.py
@@ -10,7 +10,13 @@ from .user_rate import UserRate
 from .user_rate_score import UserRateScore
 from .user_rate_status import UserRateStatus
 
-MANGAS_KIND = ['manga', 'manhwa', 'manhua', 'one_shot', 'doujin']
+MANGAS_KIND = (
+    'manga',
+    'manhwa',
+    'manhua',
+    'one_shot',
+    'doujin',
+)
 
 
 class MangaInfo(BaseModel):
@@ -39,25 +45,25 @@ class MangaInfo(BaseModel):
 
 class Manga(MangaInfo):
     """Represents manga entity."""
-    english: Optional[List[Optional[str]]]
-    japanese: Optional[List[Optional[str]]]
-    synonyms: Optional[List[Optional[str]]]
+    english: List[Optional[str]]
+    japanese: List[Optional[str]]
+    synonyms: List[str]
     license_name_ru: Optional[str]
     description: Optional[str]
-    description_html: Optional[str]
+    description_html: str
     description_source: Optional[str]
     franchise: Optional[str]
-    favoured: Optional[bool]
-    anons: Optional[bool]
-    ongoing: Optional[bool]
+    favoured: bool
+    anons: bool
+    ongoing: bool
     thread_id: Optional[int]
     topic_id: Optional[int]
-    myanimelist_id: Optional[int]
-    rates_scores_stats: Optional[List[UserRateScore]]
-    rates_statuses_stats: Optional[List[UserRateStatus]]
-    licensors: Optional[List[str]]
-    genres: Optional[List[Genre]]
-    publishers: Optional[List[Publisher]]
+    myanimelist_id: int
+    rates_scores_stats: List[UserRateScore]
+    rates_statuses_stats: List[UserRateStatus]
+    licensors: List[str]
+    genres: List[Genre]
+    publishers: List[Publisher]
     user_rate: Optional[UserRate]
 
 
