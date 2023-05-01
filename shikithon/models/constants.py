c5f56783b7e179b588af94fda8ef92ec97c6412c
SecondThundeR <awayfromgalaxy@gmail.com>
2023-05-01 19:36:21 +0300

    feat(models): update contants.py
    Use Literal for type hinting constants values from API*
    * Except for SmileyConstant, that would be pain to type and they can change more frequently

--- shikithon/models/constants.py
+++ shikithon/models/constants.py
@@ -1,31 +1,49 @@
 """Model for /api/constants"""
-from typing import List
+from typing import Tuple, Literal
 
 from pydantic import BaseModel
 
 
 class AnimeConstants(BaseModel):
     """Represents anime constants."""
-    kind: List[str]
-    status: List[str]
+    kind: Tuple[Literal['tv'], Literal['movie'], Literal['ova'], Literal['ona'],
+                Literal['special'], Literal['music']]
+    status: Tuple[Literal['anons'], Literal['ongoing'], Literal['released']]
 
 
 class MangaConstants(BaseModel):
     """Represents manga constants."""
-    kind: List[str]
-    status: List[str]
+    kind: Tuple[Literal['manga'], Literal['manhwa'], Literal['manhua'],
+                Literal['light_novel'], Literal['novel'], Literal['one_shot'],
+                Literal['doujin']]
+    status: Tuple[Literal['anons'], Literal['ongoing'], Literal['released'],
+                  Literal['paused'], Literal['discontinued']]
 
 
 class UserRateConstants(BaseModel):
     """Represents user rate constants."""
-    status: List[str]
+    status: Tuple[Literal['planned'], Literal['watching'],
+                  Literal['rewatching'], Literal['completed'],
+                  Literal['on_hold'], Literal['dropped']]
 
 
 class ClubConstants(BaseModel):
     """Represents clubs constants."""
-    join_policy: List[str]
-    comment_policy: List[str]
-    image_upload_policy: List[str]
+    join_policy: Tuple[
+        Literal['free'],
+        Literal['member_invite'],
+        Literal['admin_invite'],
+        Literal['owner_invite'],
+    ]
+    comment_policy: Tuple[
+        Literal['free'],
+        Literal['members'],
+        Literal['admins'],
+    ]
+    image_upload_policy: Tuple[
+        Literal['members'],
+        Literal['admins'],
+    ]
 
 
 class SmileyConstant(BaseModel):
